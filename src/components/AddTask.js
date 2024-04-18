8d47552c74d4b03feb77ebefc878e4e2de5e8cd5
21120551 <damtutam@gmail.com>
2024-04-18 23:08:28 +0700

    setting up component

---
+++ src/components/AddTask.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function AddTask({onAddTask}){
+    const [text, setText] = useState('');
+
+    return(
+        <>
+            <input type="text" onChange={(e) => setText(e.target.value)} />
+            <button onClick={() => {
+                setText('');
+                onAddTask(text)
+            }}>Add Task</button>
+        </>
+    )
+}
