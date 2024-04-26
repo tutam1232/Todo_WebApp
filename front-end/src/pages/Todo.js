151e9b0d6b73f3765c7702da76d9d415d3bebad5
21120551 <damtutam@gmail.com>
2024-04-26 10:41:39 +0700

    reorder on database

--- front-end/src/pages/Todo.js
+++ front-end/src/pages/Todo.js
@@ -1,46 +1,66 @@
 import TaskList from "../components/TaskList.js"
 import AddTask from "../components/AddTask.js"
-import { useTasksDispatch } from "../contexts/TasksContext.js";
-import {DragDropContext, Droppable} from 'react-beautiful-dnd';
-
+import { useTasksDispatch, useTasks } from "../contexts/TasksContext.js";
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+const API_URL = process.env.REACT_APP_API_URL
 
 function Todo() {
 
     console.log("[Todo]")
-    
-    let dispatch = useTasksDispatch();
 
-    function handleOnDragEnd(result) {
+    let dispatch = useTasksDispatch();
+    let tasks = useTasks()
+
+    async function handleOnDragEnd(result) {
 
         if (!result.destination) return;
-        dispatch({
-            type: 'reorder_task',
-            dragIndex: result.source.index,
-            dropIndex: result.destination.index
+
+        const draggedTask = tasks[result.source.index]
+        const previousDropTask = tasks[result.destination.index]
+        const dragIdDatabase = draggedTask.id;
+        const dropIdDatabase = previousDropTask.id;
+
+        let resultFetch = await fetch(API_URL + '/reorder' + `/${dragIdDatabase}` + `/${dropIdDatabase}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
         })
-       
+
+        if (resultFetch.ok) {
+            dispatch({
+                type: 'reorder_task',
+                dragIndex: result.source.index,
+                dropIndex: result.destination.index
+            })
+        }
+        else {
+            console.log('reorder fail')
+        }
+
+
     }
 
     return (
-        <>      
+        <>
             <h1 style={{ textAlign: "center", color: "white", marginTop: "2%" }}>TODO LIST</h1>
-            
-                <div style={{ width: "100%", display: "flex", justifyContent: "center" }}><AddTask /></div>
-                <DragDropContext onDragEnd={handleOnDragEnd}>
-                    <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
-                        <Droppable droppableId="tasks">
-                            {(provided) => (
-                                <div
-                                    ref={provided.innerRef}
-                                    {...provided.droppableProps} >
-                                    <TaskList />
-                                    {provided.placeholder}
-                                </div>
-                            )}
-                        </Droppable>
-                    </div>
 
-                </DragDropContext>
+            <div style={{ width: "100%", display: "flex", justifyContent: "center" }}><AddTask /></div>
+            <DragDropContext onDragEnd={handleOnDragEnd}>
+                <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+                    <Droppable droppableId="tasks">
+                        {(provided) => (
+                            <div
+                                ref={provided.innerRef}
+                                {...provided.droppableProps} >
+                                <TaskList />
+                                {provided.placeholder}
+                            </div>
+                        )}
+                    </Droppable>
+                </div>
+
+            </DragDropContext>
         </>
     );
 
