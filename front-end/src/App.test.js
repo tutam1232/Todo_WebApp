1881457583bcece6a744e28d2cd9749733716b4a
21120551 <damtutam@gmail.com>
2024-08-26 15:50:35 +0700

    fix render FE

--- front-end/src/App.test.js
+++ front-end/src/App.test.js
@@ -2,11 +2,10 @@ import { render, screen } from "@testing-library/react";
 import App from ".";
 
 test("renders login", () => {
-  const mockUpObject = {
-    focus: () => null,
-  };
-  global.document.getElementById = jest.fn(() => mockUpObject);
-  render(<App />);
+  const container = document.createElement("div");
+  container.id = "root";
+
+  render(<App />, { container: document.body.appendChild(container) });
   const linkElement = screen.getByText(/Login/i);
   expect(linkElement).toBeInTheDocument();
 });
